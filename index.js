78cbafb4e75d9faee68f29d9b4ba8376e83c432f
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-04-15 17:35:45 -0500

    add part 1 of app up to section 2 and starting section 3

--- index.js
+++ index.js
@@ -1,4 +1,37 @@
 document.addEventListener('DOMContentLoaded', function(){
+  let content = document.getElementById("movies");
+  content.innerHTML = renderMovies(movieData);
+  let movieList = [];
+  document.getElementById('search-form').addEventListener('keyup', function(e){
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target.value);
+    const searchString = e.target.value.toLowerCase();
+    const filteredMovies = movieList.filter(movie =>{
+      return movie.Title.toLowerCase().includes(searchString) || movie.Year.toLowerCase().includes(searchString);
+    });
+    document.getElementById("search-form").addEventListener('submit', function(){
+      renderMovies(filteredMovies);
+    })
+  })
 
-  function renderMovies()
+  function renderMovies(movieArray){
+    const movies = movieArray.map((item, index) => {
+      const printed =`
+      <div class="movie">
+        <div class="card" id="movie${index}" style="width: 18rem;">
+        <img src="${item.Poster}" class="card-img-top" alt="${item.Title}">
+          <div class="card-body">
+            <h5 class="card-title">${item.Title}</h5>
+            <p class="card-text">${item.Year}</p>
+            <a href="#" class="btn btn-primary">Add</a>
+          </div>
+        </div>
+      </div>
+      `;
+      console.log("this has run the render");
+      return printed;
+    });
+    return movies.join('');
+  };
 });
