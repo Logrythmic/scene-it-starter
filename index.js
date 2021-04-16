fc543e2a1fd9398d5c4d41ae16fe02dbe48871b1
Luqmaan Bradford <l.h.bradford@gmail.com>
2021-04-16 18:24:12 -0500

    add the search function to the page

--- index.js
+++ index.js
@@ -1,37 +1,41 @@
 document.addEventListener('DOMContentLoaded', function(){
-  let content = document.getElementById("movies");
-  content.innerHTML = renderMovies(movieData);
-  let movieList = [];
-  document.getElementById('search-form').addEventListener('keyup', function(e){
-    e.preventDefault();
-    console.log(e);
-    console.log(e.target.value);
-    const searchString = e.target.value.toLowerCase();
-    const filteredMovies = movieList.filter(movie =>{
-      return movie.Title.toLowerCase().includes(searchString) || movie.Year.toLowerCase().includes(searchString);
-    });
-    document.getElementById("search-form").addEventListener('submit', function(){
-      renderMovies(filteredMovies);
-    })
-  })
+  let movieBox = document.getElementById("movies");
+  movieBox.innerHTML = renderMovies(movieData);
 
-  function renderMovies(movieArray){
-    const movies = movieArray.map((item, index) => {
-      const printed =`
-      <div class="movie">
-        <div class="card" id="movie${index}" style="width: 18rem;">
-        <img src="${item.Poster}" class="card-img-top" alt="${item.Title}">
-          <div class="card-body">
-            <h5 class="card-title">${item.Title}</h5>
-            <p class="card-text">${item.Year}</p>
-            <a href="#" class="btn btn-primary">Add</a>
-          </div>
+  document.getElementById('search-form').addEventListener('keyup', function(e){ //keyup is registering the key strokes
+    e.preventDefault();
+    console.log(e); // test log
+    console.log(e.target.value); // test log
+    const searchString = e.target.value.toLowerCase(); // this stores the search terms
+    const filteredMovies = movieData.filter(movie =>{
+      return movie.Title.toLowerCase().includes(searchString) || movie.Year.toLowerCase().includes(searchString);
+    }); // this will store items that match the items searched for
+    console.log(filteredMovies); //shows the filtered movie list
+    
+    movieBox.innerHTML = renderMovies(filteredMovies); // !!Should!! render new movie list
+    console.log("search button is pressed"); //test log
+    console.log(e.target);// test log
+  });
+
+});
+
+function renderMovies(movieArray){
+  const movies = movieArray.map((item, index) => {
+    const printed =`
+    <div class="movie">
+      <div class="card" id="movie${index}" style="width: 18rem;">
+      <img src="${item.Poster}" class="card-img-top vert" alt="${item.Title}">
+        <div class="card-body">
+          <h6 class="card-title">${item.Title}</h6>
+          <p class="card-text">${item.Year}</p>
+          <a href="#" class="btn btn-primary">Add</a>
         </div>
       </div>
-      `;
-      console.log("this has run the render");
-      return printed;
-    });
-    return movies.join('');
-  };
-});
+    </div>
+    `;
+    console.log("this has run the render");
+    return printed;
+  });
+  console.log("render is showing");
+  return movies.join('');
+};
